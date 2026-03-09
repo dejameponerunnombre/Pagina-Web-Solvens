ba41d8a107b7d3c8172b2893b70f814bee1e8c02
MatyBotta <68671511+MatyBotta@users.noreply.github.com>
2026-03-09 02:08:44 -0300

    Cambio en la conexion y diseño
    Hice (o creo que funciona) la conexion para que no se sature.
    Además cambié un devuelta el mensaje para que presione registrar visita y toqué apenas el carrusel de imagenes.
    Tambien comenté la parte para cargar los productos en la carga de la visita

--- CODIGO/js/conexion.mjs
+++ CODIGO/js/conexion.mjs
@@ -2,21 +2,38 @@ import mssql from "mssql";
 
 const connectionSettings = {
     server: "localhost",
-    port: 1433,
     database: "Sistema_Solvens",
     user: "sa",
     password: "botta2212",
     options: {
+        instanceName: "SQLEXPRESS",
         trustServerCertificate: true,
         encrypt: false
-    }
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    connectionTimeout: 30000,
+    requestTimeout: 30000
 };
+
+let pool = null;
+
 export async function getConnection() {
     try {
-        return await mssql.connect(connectionSettings);
-    }
-    catch (error) {
-        console.error("Error de conexión a SQL Server:", error);
+
+        if (!pool) {
+            pool = await mssql.connect(connectionSettings);
+            console.log("✅ Conectado a SQL Server");
+        }
+
+        return pool;
+
+    } catch (error) {
+        console.error("❌ Error de conexión a SQL Server:", error);
+        throw error;
     }
 }
 
